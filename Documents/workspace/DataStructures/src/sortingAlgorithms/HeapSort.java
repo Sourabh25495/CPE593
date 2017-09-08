913a0451377e6f962410e9a65af622c472edff94
sourabh kulkarni <skulkar6@stevens.edu>
2017-09-08 17:41:59 -0400

    Heap

--- Documents/workspace/DataStructures/src/sortingAlgorithms/HeapSort.java
+++ Documents/workspace/DataStructures/src/sortingAlgorithms/HeapSort.java
@@ -16,33 +16,54 @@ public class HeapSort {
 			for(i=0;i<a.length;i++){
 				a[i]=sc.nextInt();
 			}
+			heapSort(a);
+			
+			
+			System.out.print("Sorted array ");
+			
+			for( i=0;i<a.length;i++){
+				System.out.print(a[i]+" ");// print final array
+			}
 
 	}
-	public void makeHeap(int a[]){
+	public static void makeHeap(int a[]){
 		int n=a.length;
-		for(int i=n/2;i>0;i--){
+		for(int i=n/2;i>=0;i--){
 			makeSubHeap(a,i);
 		}
 		
 	}
-	public void makeSubHeap(int a[],int i){
+	public static void makeSubHeap(int a[],int i){
 		int temp=0;
-		if(a[2*i+1] > a[2*i+2])
-			if(a[i]>a[2*i+1])
+		if(a[2*i+1] >=a[2*i+2])
+			if(a[i]<a[2*i+1]){
 				temp=a[i];
 				a[i]=a[2*i+1];
 				a[2*i+1]=temp;
 				makeSubHeap(a,2*i+1);
+			}
+			
+			else if(a[i]<a[2*i+2]){
+					temp=a[i];
+					a[i]=a[2*i+2];
+					a[2*i+2]=temp;
+					makeSubHeap(a,2*i+2);
+					
+				}
+	
+			
+		}
+	public static void heapSort(int a[]){
+		int n=a.length;
+			makeHeap(a);
+			for(int i=n-1;i>=1;i--){
+				int temp=a[0];
+				a[0]=a[i];
+				a[i]=temp;
+				makeSubHeap(a,0);
+			}
 					
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		
 
 }
