8e8575d348740d12cb267aba0a32aea28268b41a
sourabh kulkarni <skulkar6@stevens.edu>
2017-09-08 16:42:06 -0400

    Merge

--- Documents/workspace/DataStructures/src/sortingAlgorithms/HeapSort.java
+++ Documents/workspace/DataStructures/src/sortingAlgorithms/HeapSort.java
@@ -1,10 +1,48 @@
 package sortingAlgorithms;
 
+import java.util.Scanner;
+
 public class HeapSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			int n;
+			
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter the size of array");
+			n=sc.nextInt();
+			int i;
+			int[] a=new int[n];
+			for(i=0;i<a.length;i++){
+				a[i]=sc.nextInt();
+			}
 
 	}
+	public void makeHeap(int a[]){
+		int n=a.length;
+		for(int i=n/2;i>0;i--){
+			makeSubHeap(a,i);
+		}
+		
+	}
+	public void makeSubHeap(int a[],int i){
+		int temp=0;
+		if(a[2*i+1] > a[2*i+2])
+			if(a[i]>a[2*i+1])
+				temp=a[i];
+				a[i]=a[2*i+1];
+				a[2*i+1]=temp;
+				makeSubHeap(a,2*i+1);
+					
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
